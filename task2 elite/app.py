554cde16cf6e6c0656ce75b176999af40198f8c6
Yashik Jain <yashikjain.160805@gmail.com>
2025-06-30 19:28:17 +0530

    Add files via upload

---
+++ task2 elite/app.py
@@ -0,0 +1,19 @@
+import speech_recognition as sr
+
+def transcribe_audio(file_path):
+    recognizer = sr.Recognizer()
+    with sr.AudioFile(file_path) as source:
+        print("Listening...")
+        audio = recognizer.record(source)
+        print("Transcribing...")
+        try:
+            text = recognizer.recognize_google(audio)
+            print("Transcription:", text)
+        except sr.UnknownValueError:
+            print("Could not understand audio.")
+        except sr.RequestError as e:
+            print("Could not request results:", e)
+
+# Example usage
+transcribe_audio(r"C:\Users\yashi\Downloads\tech elite task2\speech_env\recordedaudio.wav")
+
